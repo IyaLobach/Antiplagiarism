6327f8c4047e6066f7012b0ebc73aff195309f11
IyaLobach <lobach.iya@gmail.com>
2023-04-09 23:30:07 +0300

    Add text vector

--- BERT.py
+++ BERT.py
@@ -18,26 +18,21 @@ import time
 
 start = time.time()
 
-# из примера
-#df = pd.read_csv('https://github.com/clairett/pytorch-sentiment-classification/raw/master/data/SST2/train.tsv', delimiter='\t', header=None)
-df = pd.read_csv('C:/Users/iyush/PycharmProjects/BERT/dataset/Text.csv', header=None, sep=";")
-print(df)
+text = pd.read_csv('C:/Users/iyush/PycharmProjects/BERT/dataset/Text.csv', header=None, sep=";")
+# text[3] = None
+print(text[2])
 
-## Хотите BERT вместо distilBERT? Раскомментируйте следующую строку:
-#model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
-# model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel, ppb.DistilBertTokenizer, 'distilbert-base-uncased')
-#
-# # Загрузка предобученной модели/токенизатора
-# tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-# model = model_class.from_pretrained(pretrained_weights)
+# pair = pd.read_csv('C:/Users/iyush/PycharmProjects/BERT/dataset/plagiarism.csv', header=None, sep=";")
+# print(pair)
 
-# исходник https://github.com/jalammar/jalammar.github.io/blob/master/notebooks/bert/A_Visual_Notebook_to_Using_BERT_for_the_First_Time.ipynb
-
-#для русского
+text_vector = pd.read_csv('C:/Users/iyush/PycharmProjects/BERT/dataset/Text_vector.csv', header=None, sep=",")
+print(text_vector[3])
+# # для русского
 # tokenizer = AutoTokenizer.from_pretrained("Geotrend/distilbert-base-ru-cased")
 # model = AutoModel.from_pretrained("Geotrend/distilbert-base-ru-cased")
 #
-# tokenized = df[1].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
+# # токенизация
+# tokenized = text[2].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
 #
 #
 # max_len = 0
@@ -45,26 +40,38 @@ print(df)
 #     if len(i) > max_len:
 #         max_len = len(i)
 #
-# padded = np.array([i + [0]*(max_len-len(i)) for i in tokenized.values])
+#
+# print(max_len)
+#
+# if max_len < 512:
+#     padded = np.array([i + [0]*(512-len(i)) for i in tokenized.values])
+# else:
+#     print("max_len > 512")
 #
 # input_ids = torch.tensor(np.array(padded))
 # with torch.no_grad():
 #     last_hidden_states = model(input_ids)
-
-
-
+#
+#
+#
 # # Разрежьте выход для первой позиции во всех последовательностях, возьмите все выходы скрытых нейронок
 # features = last_hidden_states[0][:,0,:].numpy()
-end = time.time()
-# #
-# # print("Количество предложений", len(features))
-# # print("Размер вектора", len(features[0]))
-# # is_similar = dot(features[4], features[5])/(norm(features[4])*norm(features[5]))
-# # print(is_similar)
-# # is_similar = dot(features[1], features[2])/(norm(features[1])*norm(features[2]))
-# # print(is_similar)
-# # is_similar = dot(features[1], features[3])/(norm(features[1])*norm(features[3]))
-# # print(is_similar)
+#
+# vectors = []
+# for i in features:
+#     tmp = []
+#     for j in i:
+#         tmp.append(j)
+#     vectors.append(tmp)
+# text[3] = vectors
+# print(text)
+# text.to_csv(r'C:/Users/iyush/PycharmProjects/BERT/dataset/Text_vector.csv', index=False, header=None)
+#
+#
+# print("Количество строк", len(features))
+# print("Размер вектора", len(features[0]))
 
+
+end = time.time()
 print("Время ", (end - start)/60)
 
